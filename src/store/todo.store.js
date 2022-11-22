6d4086e57868c51c31f25d09d4e301867ef7fa98
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-22 15:01:23 -0500

    antes del toggle

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -25,12 +25,31 @@ const loadStore = () => {
     throw new Error('Not implemented');
 }
 
+
+const getTodos = ( filter = Filters.All ) => {
+    
+    switch( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+}
+
 /**
  * 
  * @param {String} description 
  */
 const addTodo = ( description ) => {
-    throw new Error('Not implemented');
+    if ( !description ) throw new Error('Description is required');
+    state.todos.push( new Todo(description) );
 }
 
 /**
@@ -42,26 +61,32 @@ const toggleTodo = ( todoId ) => {
 }
 
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId  );
 }
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.done );
 }
 
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
 const setFilter = ( newFilter = Filters.All ) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 
 export default {
+    addTodo,
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
