fdd22c0a56a6843ecd091397614fa32e87b8b095
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-22 14:42:16 -0500

    antes de store

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,4 +1,4 @@
-
+import { v4 as uuid } from 'uuid';
 
 export class Todo {
 
@@ -7,7 +7,7 @@ export class Todo {
      * @param {String} description 
      */
     constructor( description ){
-        this.id = 1;
+        this.id = uuid();
         this.description = description;
         this.done = false;
         this.createdAt = new Date();        
